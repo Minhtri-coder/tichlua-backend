9113a6e132b4fbe32dd88fc6a230b35b692f1882
MinhTri <vodaiminhtri@gmail.com>
2026-09-23 11:27:15 +0700

    finish backend

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ dotenv.config();
 
 
 const app: Express = express();
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
